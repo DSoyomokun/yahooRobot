ba8134cfbdf27865553fa6320a30ce77ef08b143
nick0npluto <118879710+nick0npluto@users.noreply.github.com>
2025-11-19 18:50:56 -0500

    Add movement functions for driving in inches

--- robot_control/movement.py
+++ robot_control/movement.py
@@ -1,1 +1,13 @@
+from robot_control.config import *
+import math
+
+wheel_circ = math.pi * WHEEL_DIAMETER_IN
+
+def inches_to_ticks(inches):
+    rotations = inches / wheel_circ
+    return rotations * TICKS_PER_ROT
+
+def drive_inches(inches):
+    ticks = inches_to_ticks(inches)
+    motor.drive_ticks(ticks)   # you replace with real robot API
 
